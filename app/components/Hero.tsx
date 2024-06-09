7ebe6f004c21ce4672e2c2656dc29acf57d4f2a2
Edson Dev <127622832+XO-Edson@users.noreply.github.com>
2024-06-09 14:48:08 +0300

    added new resume link

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -42,7 +42,7 @@ function Hero() {
           </p>
           <Button>
             <a
-              href="https://drive.google.com/file/d/1pQFE1_-R8vj8vBe_nWFTY_BjlDmV72oB/view?usp=sharing"
+              href="https://drive.google.com/file/d/1gRbfDuBcrWYy9Q7Wr3wlyyPlaLjeEE_0/view?usp=sharing"
               target="_blank"
             >
               Resume
